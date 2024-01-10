ac32b04e76421f5789f814bbed07f9c1246b18b0
Lucie Vincent <lucie.sarah.vincent@gmail.com>
2024-01-10 13:54:18 +0100

    organisation

--- index.php
+++ index.php
@@ -4,9 +4,18 @@ spl_autoload_register(function ($class_name) {
     include $class_name . '.php';
 });
 
+// réalisateurs
 $ridleyScott = new Realisateur("Ridley", "SCOTT", "Masculin", 1937);
-$BladeRunner = new Film("Blade Runner", 1982, 117, $ridleyScott, "SF");
-$LeDernierDuel = new Film("Le Dernier Duel", 2021, 152, $ridleyScott, "Drame Historique");
+
+// acteurs
 $susanSarandon = new Acteur("Susan", "SARANDON", "Feminin", 1946);
+
+// films
+$BladeRunner = new Film("Blade Runner", 1982, 117, $ridleyScott, "SF");
+$LeDernierDuel = new Film("Le Dernier Duel", 2021, 152, $ridleyScott, "Drame historique");
+$ThelmaLouise = new Film("Thelma et Louise", 1991, 129, $ridleyScott, "Road movie");
+
+//appel de fonctions
 echo $ridleyScott->afficherFilmographie();
-echo $susanSarandon->afficherFilmographie();
+// $susanSarandon->addFilm($ThelmaLouise);
+// echo $susanSarandon->afficherFilmographie();
